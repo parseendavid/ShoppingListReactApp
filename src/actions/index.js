1c1e3ac29a32a085f84a5307dade5bb7ed6701a0
David Parseen Maitoyo <david.maitoyo@andela.com>
2018-01-04 15:23:56 +0300

    [Feature] Finished CRUD for shopping lists.

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,11 @@ import {toast} from "react-toastify";
 export const FETCH_SHOPPING_LISTS = "fetch_shopping_lists";
 export const FETCH_SHOPPING_LIST_ITEMS = "fetch_shopping_list_items";
 export const FETCH_JWT_TOKEN = "fetch_jwt_token";
-
+///////////////////////////////////////////
+//                                      //
+//      AUTHENTICATION ACTIONS          //
+//                                       //
+//////////////////////////////////////////
 export function SignUp_User(values) {
     return (dispatch) => {
         axiosConfig.request({
@@ -12,14 +16,14 @@ export function SignUp_User(values) {
             url: `/auth/register/`,
             data: values
         })
-        .then((response) => {
-            toast.info(response.data.message);
-        })
-        .catch(
-            error =>{
-                //
-            }
-        );
+            .then((response) => {
+                toast.info(response.data.message);
+            })
+            .catch(
+                error => {
+                    toast.warn(error.response.data.message);
+                }
+            );
     };
 }
 
@@ -30,19 +34,24 @@ export function Login_User(values) {
             url: `/auth/login/`,
             data: values
         })
-        .then((response) => {
-            toast.success(response.data.message);
-            dispatch({
-                type: FETCH_JWT_TOKEN,
-                payload: response
+            .then((response) => {
+                toast.success(response.data.message);
+                dispatch({
+                    type: FETCH_JWT_TOKEN,
+                    payload: response
+                });
+            })
+            .catch(() => {
+                toast.error("INVALID CREDENTIALS !!!!");
             });
-        })
-        .catch(()=>{
-            toast.error("INVALID CREDENTIALS !!!!");
-            });
-        };
+    };
 }
 
+///////////////////////////////////////////
+//                                      //
+//      SHOPPING LISTS ACTIONS          //
+//                                       //
+//////////////////////////////////////////
 export function Fetch_Shopping_Lists() {
     return (dispatch) => {
         axiosConfig.request({
@@ -56,11 +65,85 @@ export function Fetch_Shopping_Lists() {
                 type: FETCH_SHOPPING_LISTS,
                 payload: response
             });
-        });
+        })
+            .catch(
+                error => {
+                    switch (error.response.status) {
+                        case 401:
+                            localStorage.removeItem("TOKEN");
+                            window.location.href = "/login";
+                    }
+                }
+            );
     };
 }
 
-
+export function Add_Shopping_List(values) {
+    return function (dispatch) {
+        axiosConfig.request({
+            method: "post",
+            url: `/lists`,
+            headers: {
+                "x-access-token": localStorage.getItem("TOKEN")
+            },
+            data: values
+        }).then((response) => {
+            dispatch(Fetch_Shopping_Lists());
+            toast.success(response.data.message);
+            $("#add-list-modal").modal("close");
+        }).catch(
+            error => {
+                toast.error(error.response.data.message);
+                return error;
+            }
+        );
+    };
+}
+export function Edit_Shopping_List(values) {
+    return function (dispatch) {
+        axiosConfig.request({
+            method: "put",
+            url: `/list/${values.id}`,
+            headers: {
+                "x-access-token": localStorage.getItem("TOKEN")
+            },
+            data: values
+        }).then((response) => {
+            dispatch(Fetch_Shopping_Lists());
+            toast.success(response.data.message);
+            $("#edit-list-modal").modal("close");
+        }).catch(
+            error => {
+                toast.error(error.response.data.message);
+                return error;
+            }
+        );
+    };
+}
+export function Delete_Shopping_List(id) {
+    return function (dispatch) {
+        axiosConfig.request({
+            method: "delete",
+            url: `/list/${id}`,
+            headers: {
+                "x-access-token": localStorage.getItem("TOKEN")
+            },
+        }).then((response) => {
+            dispatch(Fetch_Shopping_Lists());
+            toast.warn(response.data.message);
+        }).catch(
+            error => {
+                toast.error(error.response.data.message);
+                return error;
+            }
+        );
+    };
+}
+///////////////////////////////////////////
+//                                      //
+//      LIST ITEMS ACTIONS              //
+//                                       //
+//////////////////////////////////////////
 export function Fetch_Shopping_List_Items(store) {
     return (dispatch) => {
         axiosConfig.request({
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,15 +1,48 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import _ from 'lodash';
+import {bindActionCreators} from "redux";
 
 import NavigationBar from "./nav";
-import {Fetch_Shopping_Lists} from "../actions";
+import {Delete_Shopping_List,Edit_Shopping_List,Add_Shopping_List,Fetch_Shopping_Lists} from "../actions";
 import PropTypes from "prop-types";
 
 
 class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state={list_name:"", edit_list:{name:"",id:""}};
+        this.handleAddList = this.handleAddList.bind(this);
+        this.handleEditValues = this.handleEditValues.bind(this);
+        this.handleEditList = this.handleEditList.bind(this);
+        this.handleDeleteList = this.handleDeleteList.bind(this);
+
+
+    }
+
     componentWillMount() {
-        this.props.Fetch_Shopping_Lists();
+        this.props.actions.Fetch_Shopping_Lists();
+    }
+
+    handleAddList(e){
+        e.preventDefault();
+        this.props.actions.Add_Shopping_List({"list_name":this.refs.list_name.value});
+    }
+    handleEditValues(e){
+        e.preventDefault();
+        let edit_list = this.state.edit_list;
+        edit_list["new_name"] = e.target.value;
+        this.setState(edit_list);
+    }
+
+    handleEditList(e){
+        e.preventDefault();
+        this.props.actions.Edit_Shopping_List({"id":this.refs.list_id.value,"list_name":this.refs.new_name.value});
+    }
+
+    handleDeleteList(e,id){
+        e.preventDefault();
+        this.props.actions.Delete_Shopping_List(id);
     }
 
     renderListTableDetails() {
@@ -34,12 +67,19 @@ class Dashboard extends Component {
                                         <a className="waves-effect waves-light" href={`/list/${list.id}`}>details</a>
                                     </td>
                                     <td>
-                                        <a className="waves-effect waves-light" href="#">
+                                        <a className="modal-trigger waves-effect waves-light" href="#edit-list-modal"
+                                        onClick={()=>{this.setState({"edit_list":
+                                            {
+                                            "name":list.list_name,"id":list.id
+                                            }
+                                        });
+                                        }}>
                                             <i className="default-text-color material-icons right">edit</i>
                                         </a>
                                     </td>
                                     <td>
-                                        <a className="waves-effect waves-light" href="#">
+                                        <a className="waves-effect waves-light"
+                                        onClick={e=>this.handleDeleteList(e,list.id)}>
                                             <i className="delete-text-color material-icons right">delete_sweep</i>
                                         </a>
                                     </td>
@@ -54,26 +94,98 @@ class Dashboard extends Component {
 
     render() {
         return (
-            <div>
-                <NavigationBar text="logout" icon="input" link="/logout" home_link="/dashboard"/>
-                <div className="container badge">
-                    <h6>Dashboard</h6>
-                    {this.renderListTableDetails()}
-                    <a className="right btn-floating btn-large waves-effect waves-light accent-color"><i
-                        className="material-icons">add</i></a>
+                <div>
+                    <NavigationBar text="logout" icon="input" link="/logout" home_link="/dashboard"/>
+                    <div className="container badge">
+                        <h6>Dashboard</h6>
+                        <a className="modal-trigger right btn-floating btn-large waves-effect waves-light accent-color"
+                           href="#add-list-modal">
+                            <i className="material-icons">add</i>
+                        </a>
+                        <div id="add-list-modal" className="modal">
+                            <a className="modal-action modal-close waves-effect waves-green btn-flat right ">
+                                <i className="material-icons red">close</i>
+                            </a>
+                            <div className="modal-content">
+                                <h6>Add A New Shopping List</h6>
+                                <form onSubmit={this.handleAddList}>
+                                    <div>
+                                        <input ref="list_name"
+                                               type="text"
+                                               placeholder="List Name"
+                                               pattern="^[a-zA-Z0-9_]*$"
+                                               title="Must not contain special characters"
+                                               required/>
+                                        <div className="modal-footer">
+                                            <button className={"btn waves-effect waves-light light-blue right"}
+                                                    type="submit">
+                                                ADD
+                                            </button>
+                                            <button className={"btn waves-effect waves-light grey left"}
+                                                    type="reset">
+                                                RESET
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div id="edit-list-modal" className="modal">
+                            <a className="modal-action modal-close waves-effect waves-green btn-flat right ">
+                                <i className="material-icons red">close</i>
+                            </a>
+                            <div className="modal-content">
+                                <h6>Edit list "{this.state.edit_list.name}"</h6>
+                                <form onSubmit={this.handleEditList}>
+                                    <div>
+                                        <input ref="list_id"
+                                               value={this.state.edit_list.id}
+                                               type="text"
+                                               required
+                                               hidden
+                                        />
+                                        <input ref="new_name"
+                                               value={this.state.edit_list.new_name || this.state.edit_list.name}
+                                               onChange={this.handleEditValues}
+                                               type="text"
+                                               placeholder="New List Name"
+                                               pattern="^[a-zA-Z0-9_]*$"
+                                               title="Must not contain special characters"
+                                               required
+                                        />
+                                        <div className="modal-footer">
+                                            <button className={"btn waves-effect waves-light light-blue right"}
+                                                    type="submit">
+                                                CHANGE
+                                            </button>
+                                            <button className={"btn waves-effect waves-light grey left"}
+                                                    type="reset">
+                                                RESET
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        {this.renderListTableDetails()}
+                    </div>
                 </div>
-            </div>
-        );
+            );
     }
 }
 Dashboard.propTypes = {
     token: PropTypes.string,
-    Fetch_Shopping_Lists: PropTypes.func.isRequired,
+    actions: PropTypes.object.isRequired,
     dispatch: PropTypes.object,
-    lists: PropTypes.object
+    lists: PropTypes.object,
 };
 function mapStateToProps(state) {
     return {lists: state.lists};
 }
-
-export default connect(mapStateToProps, {Fetch_Shopping_Lists})(Dashboard);
+function mapDispatchToProps(dispatch) {
+    return {actions : bindActionCreators(
+        {
+            Fetch_Shopping_Lists, Add_Shopping_List,Edit_Shopping_List,Delete_Shopping_List
+        },dispatch)};
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Dashboard);
