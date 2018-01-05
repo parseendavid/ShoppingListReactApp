731a4b3fc9957ae08815813a88325b47de3c9057
David Parseen Maitoyo <david.maitoyo@andela.com>
2018-01-05 22:40:36 +0300

    [Feature] Used heroku api.

--- src/extras/axios_config.js
+++ src/extras/axios_config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = "https://shopping-list-api-2017.herokuapp.com";
 
 const axiosConfig = axios.create({
     baseURL: BASE_URL,
