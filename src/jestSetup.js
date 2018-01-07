dbacb63d3260c2f062ad9e24ab794c4af811c357
David Parseen Maitoyo <david.maitoyo@andela.com>
2018-01-07 13:32:59 +0300

    [Chore] Removed unused lines in jestSetup.

--- src/jestSetup.js
+++ src/jestSetup.js
@@ -14,13 +14,6 @@ let exposedProperties = ['window', 'navigator', 'document'];
 global.document = document;
 global.window = document.defaultView;
 document.localStorage = global.localStorage;
-Object.keys(document.defaultView).forEach((property) => {
-    if (typeof global[property] === 'undefined') {
-        exposedProperties.push(property);
-        global[property] = document.defaultView[property];
-    }
-});
-
 global.navigator = {
     userAgent: 'node.js'
 };
